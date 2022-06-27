e67382e1f7ce96e01a8a15a40afab97556db88ab
pmarcelojr <marcelosantostecnologia@gmail.com>
2022-06-27 20:38:45 -0300

    The sprout class technique unit testing legacy code

---
+++ SprountClass.Test/EmployeesTest.cs
@@ -0,0 +1,21 @@
+using Moq;
+using SproutClass;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace SprountClass.Test
+{
+    public class EmployeesTest
+    {
+        [Fact]
+        public void GetEmployeeData_ReturnsSalaries()
+        {
+            var currentDataObject = new Mock<IEmployeeService>();
+            currentDataObject.Setup(x => x.GetEmployeesData()).Returns(new List<Employee>() { new Employee { UniqueId = 1, Salary = 100 } });
+            var salaryData = currentDataObject.Object.GetEmployeesData();
+
+            Assert.Contains(salaryData, x => x.Salary > 0);
+        }
+    }
+}
